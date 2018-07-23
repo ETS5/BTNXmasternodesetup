a0418528e93d999632b16b274597a7f97ded15e4
Brandon <35798060+ETS5@users.noreply.github.com>
2018-07-22 22:56:45 -0500

    Update bitnexus-setup.sh

--- bitnexus-setup.sh
+++ bitnexus-setup.sh
@@ -22,7 +22,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 #Bitnexus TCP port
-PORT=6161
+PORT=11010
 
 #Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
